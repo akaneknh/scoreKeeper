6c8768bea9046bc1f06d17541591634ef315ca44
Akane Kano <bglakane@gmail.com>
2023-12-01 06:22:35 -0800

    function of winning score and disabling the buttons modified

--- app.js
+++ app.js
@@ -10,35 +10,59 @@ let p2Score = 0;
 let winningScore = 3;
 let isGameOver = false;
 
-
-p1Button.addEventListener("click", function(){
-  if(!isGameOver){
-    p1Score += 1;
-    if(p1Score == winningScore){
-     isGameOver = true;
-     p1Display.classList.add("has-text-success");
-     p2Display.classList.add("has-text-danger");
-     p1Button.disabled = true;
-     p2Button.disabled = true;
-    };
-    p1Display.textContent = p1Score;
+function playerHasWinningScore(Score) {
+  if(Score === winningScore) {
+    isGameOver = true;
+    p1Button.disabled = true;
+    p2Button.disabled = true;
+    return true;
   }
-
-})
-p2Button.addEventListener("click", function(){
-  if(!isGameOver){
-    p2Score += 1;
-    if(p2Score == winningScore ){
-     isGameOver = true;
-     p2Display.classList.add("has-text-success");
-     p1Display.classList.add("has-text-danger");
-     p1Button.disabled = true;
-     p2Button.disabled = true;
-    };
-    p2Display.textContent = p2Score;
+  return false;
+}
+p1Button.addEventListener("click", function(e) {
+  p1Score += 1;
+  if(playerHasWinningScore(p1Score)){
+    p1Display.classList.add("has-text-success");
+    p2Display.classList.add("has-text-danger");
   }
-
+  p1Display.textContent = p1Score;
 })
+// p1Button.addEventListener("click", function(){
+//   if(!isGameOver){
+//     p1Score += 1;
+//     if(p1Score == winningScore){
+//      isGameOver = true;
+//      p1Display.classList.add("has-text-success");
+//      p2Display.classList.add("has-text-danger");
+//      p1Button.disabled = true;
+//      p2Button.disabled = true;
+//     };
+//     p1Display.textContent = p1Score;
+//   }
+
+// })
+p2Button.addEventListener("click", function(e) {
+  p2Score += 1;
+  if(playerHasWinningScore(p2Score)){
+    p2Display.classList.add("has-text-success");
+    p1Display.classList.add("has-text-danger");
+  }
+  p2Display.textContent = p2Score;
+})
+// p2Button.addEventListener("click", function(){
+//   if(!isGameOver){
+//     p2Score += 1;
+//     if(p2Score == winningScore ){
+//      isGameOver = true;
+//      p2Display.classList.add("has-text-success");
+//      p1Display.classList.add("has-text-danger");
+//      p1Button.disabled = true;
+//      p2Button.disabled = true;
+//     };
+//     p2Display.textContent = p2Score;
+//   }
+
+// })
 
 winningScoreSelect.addEventListener("change", function () {
  winningScore = parseInt(this.value);
@@ -46,7 +70,6 @@ winningScoreSelect.addEventListener("change", function () {
 })
 
 resetButton.addEventListener("click", reset)
-
 function reset() {
   isGameOver = false;
   p1Score = 0;
